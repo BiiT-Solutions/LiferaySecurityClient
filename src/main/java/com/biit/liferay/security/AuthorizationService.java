da46fb681cc00b2747d0228db52ec90bf144a23f
Jorge Hortelano <jhortelano@biit-solutions.com>
2013-08-21 17:37:46 +0200

    Avoid user null.

--- src/main/java/com/biit/liferay/security/AuthorizationService.java
+++ src/main/java/com/biit/liferay/security/AuthorizationService.java
@@ -32,72 +32,83 @@ public abstract class AuthorizationService {
 	}
 
 	public boolean isAuthorizedActivity(User user, String activity) {
-		if (getUserActivitiesAllowed(user).contains(activity)) {
-			return true;
+		if (user != null) {
+			if (getUserActivitiesAllowed(user).contains(activity)) {
+				return true;
+			}
+			LiferayAuthenticationClientLogger.debug(AuthorizationService.class.getName(), "Activity " + activity
+					+ " not authorized to " + user.getEmailAddress());
 		}
-		LiferayAuthenticationClientLogger.debug(AuthorizationService.class.getName(), "Activity " + activity + " not authorized to " + user.getEmailAddress());
 		return false;
 	}
 
-	private List<String> getUserActivitiesAllowed(User currentUser) {
-		List<Role> roles = getUserRoles(currentUser);
-		List<UserGroup> userGroups = getUserGroups(currentUser);
+	private List<String> getUserActivitiesAllowed(User user) {
 		List<String> activities = new ArrayList<String>();
+		if (user != null) {
+			List<Role> roles = getUserRoles(user);
+			List<UserGroup> userGroups = getUserGroups(user);
 
-		// Add roles obtained by group.
-		for (UserGroup group : userGroups) {
-			roles.addAll(getUserGroupRoles(group));
-		}
+			// Add roles obtained by group.
+			for (UserGroup group : userGroups) {
+				roles.addAll(getUserGroupRoles(group));
+			}
 
-		// Activities by role.
-		for (Role role : roles) {
-			List<String> roleActivities = getRoleActivities(role);
-			activities.addAll(roleActivities);
+			// Activities by role.
+			for (Role role : roles) {
+				List<String> roleActivities = getRoleActivities(role);
+				activities.addAll(roleActivities);
+			}
 		}
 		return activities;
 	}
 
 	public List<Role> getUserRoles(User user) {
-		try {
-			return RoleService.getInstance().getUserRoles(user);
-		} catch (RemoteException e) {
-			LiferayAuthenticationClientLogger.error(AuthorizationService.class.getName(),
-					"Error retrieving the user's roles from '" + user.getEmailAddress() + "'");
-			LiferayAuthenticationClientLogger.errorMessage(AuthorizationService.class.getName(), e);
-		} catch (NotConnectedToWebServiceException e) {
-			LiferayAuthenticationClientLogger.error(AuthorizationService.class.getName(),
-					"Error retrieving the user's roles from '" + user.getEmailAddress() + "'");
-			LiferayAuthenticationClientLogger.errorMessage(AuthorizationService.class.getName(), e);
+		if (user != null) {
+			try {
+				return RoleService.getInstance().getUserRoles(user);
+			} catch (RemoteException e) {
+				LiferayAuthenticationClientLogger.error(AuthorizationService.class.getName(),
+						"Error retrieving the user's roles from '" + user.getEmailAddress() + "'");
+				LiferayAuthenticationClientLogger.errorMessage(AuthorizationService.class.getName(), e);
+			} catch (NotConnectedToWebServiceException e) {
+				LiferayAuthenticationClientLogger.error(AuthorizationService.class.getName(),
+						"Error retrieving the user's roles from '" + user.getEmailAddress() + "'");
+				LiferayAuthenticationClientLogger.errorMessage(AuthorizationService.class.getName(), e);
+			}
 		}
 		return new ArrayList<Role>();
 	}
 
 	public List<UserGroup> getUserGroups(User user) {
-		try {
-			return GroupService.getInstance().getUserUserGroups(user);
-		} catch (RemoteException e) {
-			LiferayAuthenticationClientLogger.error(AuthorizationService.class.getName(),
-					"Error retrieving the user's groups from " + user.getEmailAddress() + "'");
-			LiferayAuthenticationClientLogger.errorMessage(AuthorizationService.class.getName(), e);
-		} catch (NotConnectedToWebServiceException e) {
-			LiferayAuthenticationClientLogger.error(AuthorizationService.class.getName(),
-					"Error retrieving the user's groups from " + user.getEmailAddress() + "'");
-			LiferayAuthenticationClientLogger.errorMessage(AuthorizationService.class.getName(), e);
+		if (user != null) {
+			try {
+				return GroupService.getInstance().getUserUserGroups(user);
+			} catch (RemoteException e) {
+				LiferayAuthenticationClientLogger.error(AuthorizationService.class.getName(),
+						"Error retrieving the user's groups from " + user.getEmailAddress() + "'");
+				LiferayAuthenticationClientLogger.errorMessage(AuthorizationService.class.getName(), e);
+			} catch (NotConnectedToWebServiceException e) {
+				LiferayAuthenticationClientLogger.error(AuthorizationService.class.getName(),
+						"Error retrieving the user's groups from " + user.getEmailAddress() + "'");
+				LiferayAuthenticationClientLogger.errorMessage(AuthorizationService.class.getName(), e);
+			}
 		}
 		return new ArrayList<UserGroup>();
 	}
 
 	public List<Role> getUserGroupRoles(UserGroup group) {
-		try {
-			return RoleService.getInstance().getGroupRoles(group);
-		} catch (RemoteException e) {
-			LiferayAuthenticationClientLogger.error(AuthorizationService.class.getName(),
-					"Error retrieving the group's roles from " + group.getName() + "'");
-			LiferayAuthenticationClientLogger.errorMessage(AuthorizationService.class.getName(), e);
-		} catch (NotConnectedToWebServiceException e) {
-			LiferayAuthenticationClientLogger.error(AuthorizationService.class.getName(),
-					"Error retrieving the group's roles from " + group.getName() + "'");
-			LiferayAuthenticationClientLogger.errorMessage(AuthorizationService.class.getName(), e);
+		if (group != null) {
+			try {
+				return RoleService.getInstance().getGroupRoles(group);
+			} catch (RemoteException e) {
+				LiferayAuthenticationClientLogger.error(AuthorizationService.class.getName(),
+						"Error retrieving the group's roles from " + group.getName() + "'");
+				LiferayAuthenticationClientLogger.errorMessage(AuthorizationService.class.getName(), e);
+			} catch (NotConnectedToWebServiceException e) {
+				LiferayAuthenticationClientLogger.error(AuthorizationService.class.getName(),
+						"Error retrieving the group's roles from " + group.getName() + "'");
+				LiferayAuthenticationClientLogger.errorMessage(AuthorizationService.class.getName(), e);
+			}
 		}
 		return new ArrayList<Role>();
 	}
