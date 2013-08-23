ed31fb0ddab9d1c2beb8862fce7d29debc93e5f6
Jorge Hortelano <jhortelano@biit-solutions.com>
2013-08-23 11:07:59 +0200

    Now allows the use of special characters in password: ! ( ) _ -

---
+++ src/test/java/com/biit/liferay/access/AccessUtilsTest.java
@@ -0,0 +1,23 @@
+package com.biit.liferay.access;
+
+import org.testng.annotations.Test;
+
+import com.biit.liferay.access.exceptions.NotValidPasswordException;
+
+public class AccessUtilsTest {
+
+	@Test(groups = { "password" }, expectedExceptions = NotValidPasswordException.class)
+	public void notValidPassword() throws NotValidPasswordException {
+		AccessUtils.checkPassword("111@22");
+	}
+	
+	@Test(groups = { "password" }, expectedExceptions = NotValidPasswordException.class)
+	public void notValidPassword2() throws NotValidPasswordException {
+		AccessUtils.checkPassword("asd¡");
+	}
+
+	@Test(groups = { "password" })
+	public void validPassword() throws NotValidPasswordException {
+		AccessUtils.checkPassword("A!Sa(dfgH*))!120");
+	}
+}
